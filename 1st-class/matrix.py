305d2fd5afc2aef66198512b67bd088b0719481f
andresruiz <>
2024-10-27 00:40:48 -0600

    matrix multiplication working

--- 1st-class/matrix.py
+++ 1st-class/matrix.py
@@ -6,14 +6,12 @@ def multiply(A, B):
   if (not areMatricesValid(A, B)):
     return "Please enter valid matrices"
 
+  columns = matrixColumns(B)
   matrix = []
   for x in range(0, len(A)):
     vector = []
-    for y in range(0, len(A)):
-      column = []
-      for z in range(0, len(B)):
-        column.append(B[z][y])
-      vector.append(dotProduct(A[x], column))
+    for y in range(0, len(columns)):
+        vector.append(dotProduct(A[x], columns[y]))
     matrix.append(vector)
 
   return matrix
@@ -24,3 +22,14 @@ def areMatricesValid(A, B) -> bool:
     if (len(A[x]) != len(B)):
       areValid = False
   return areValid
+
+def matrixColumns(A):
+  columns = []
+  for x in range(0, len(A)):
+    for y in range(0, len(A[x])):
+      vector = []
+      for z in range(0, len(A)):
+        vector.append(A[z][y])
+      columns.append(vector)
+    break
+  return columns
